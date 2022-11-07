ffeff2b78c418ffe04cf1e55cea82c9302cb7acc
patty <patrick.celee@gmail.com>
2022-11-07 13:28:06 +0800

    fix previous problem. posteval() have new bug

--- hw3_infix_postfix/hw3_infix2postfix.c
+++ hw3_infix_postfix/hw3_infix2postfix.c
@@ -177,9 +177,9 @@ treenode* infixExpressionTree(char* expression){
             Cpush(cstack, ctop, strlen(expression), expression[i]);
         }
     }
-    free(cstack);
+    // free(cstack);
     Tnode = Npeek(nstack, ntop);
-    free(nstack);
+    // free(nstack);
     return Tnode;
 }
 
@@ -207,28 +207,47 @@ int posteval(char* expression){
                 num_stack[++top] = b * a;
                 break;
             case '/':
+                if (a == 0){
+                    printf("\nError, division by 0!");
+                    return -2147483648;
+                }
                 num_stack[++top] = b / a;
                 break;
             }
+        printf("%d %d\n", num_stack[top], top);
     }
-    return num_stack[top];
+    printf("---\n");
+    int ans = num_stack[top];
+    // free(num_stack);
+    return ans;
 }
 
 
 int valid_input(char* expression){
-    int Dflag = 0;
-    int Aflag = 0;
+    int Dflag = 0; int Aflag = 0;
+    int lparen = 0; int rparen = 0;
+    if (!isdigit(expression[0]) && !isalpha(expression[0]) && !isoperator(expression[0]) && expression[0] != '(' && expression[0] != ')'){
+        printf("%s\n", expression);
+        printf("^\n");
+        printf("Unknown Character!\n");
+        return 0;
+    }
     for (int i=1;i<strlen(expression);i++){
-        if (isalpha(expression[i])) 
-            Aflag = 1;
-        if (isdigit(expression[i]))
-            Dflag = 1;
+        // paren pairs are valid, did numbers and chars are miixed
+        lparen += ((expression[i] == '(') * 1) + ((expression[i] == ')') * 1);
+        rparen += ((expression[i] == '(') * 1) + ((expression[i] == ')') * 1);
+        Aflag = 1 * isalpha(expression[i]);
+        Dflag = 1 * isdigit(expression[i]);
         // detect unknown character
-        if (!isdigit(expression[i-1]) && !isalpha(expression[i-1]) && !isoperator(expression[i-1]) && expression[i-1] != '(' && expression[i-1] != ')'){
-            printf("%d\n",i);
+        if (!isdigit(expression[i]) && !isalpha(expression[i]) && !isoperator(expression[i]) && expression[i] != '(' && expression[i] != ')'){
             printf("%s\n", expression);
-            for (int j=0;j<i-1;j++) printf(" "); printf("^\n");
-            for (int j=0;j<i-1;j++) printf(" "); printf("Unknown Character!\n");
+            for (int j=0;j<i;j++) printf(" "); printf("^\n");
+            for (int j=0;j<i;j++) printf(" "); printf("Unknown Character!\n");
+            return 0;
+        }
+        // expression start/end with operator
+        if (isoperator(expression[0]) || isoperator(expression[strlen(expression)-1])){
+            printf("Expression should not start/end with operator!\n");
             return 0;
         }
         // detect continuos operand or operator
@@ -238,14 +257,16 @@ int valid_input(char* expression){
             for (int j=0;j<i;j++) printf(" "); printf("Error Input!\n");
             return 0;
         }
-        // expression start/end with operator
-        if (isoperator(expression[0]) || isoperator(expression[strlen(expression)-1])){
-            printf("Expression should not start/end with operator!\n");
-            return 0;
-        }
     }
+    lparen += ((expression[strlen(expression)-1] == '(') * 1) + ((expression[strlen(expression)-1] == ')') * 1);
+    rparen += ((expression[strlen(expression)-1] == '(') * 1) + ((expression[strlen(expression)-1] == ')') * 1);
     if (Dflag && Aflag){
         printf("Don't mix number with characters!\n");
+        return 0;
+    }
+
+    if (lparen != rparen){
+        printf("Parens did not match!");
         return 0;
     }
     return 1;
@@ -257,18 +278,21 @@ int digit_input(char* expression){
     for (int i=0;i<strlen(expression);i++){
         if (expression[i] == '(' || expression[i] == ')' || isoperator(expression[i]))
             continue;
-        return isdigit(expression[i]);
+        if (isdigit(expression[i])){
+            return 1;
+        }
     }
+    return 0;
 }
 
 
+// 8*((4+2)/(5-3))+4/(4+(2-5))  ((a+(b-k))*((m/(n*p)+n)/(d+e)))
 int main(){
-    // char* expression = "8*((4+2)/(5-3))+4/(4+(2-5))";
-    // char* expression = "((a+(b-k))*((m/(n*p)+n)/(d+e)))";
-    char* expression = (char*)malloc(sizeof(char));
+    char* post = (char*)malloc(sizeof(char) * (40)); char** ptrans = &post;
     while (1){
+        char* expression = (char*)malloc(sizeof(char) * 40);
         printf("\nPlease enter an infix expression and press enter: "); scanf("%s",expression);
-        if (expression[0] == 27)
+        if (expression[0] == 27)    // esc then quit program (esc ASCII is 27)
             break;
 
         if (valid_input(expression)){
@@ -276,15 +300,19 @@ int main(){
             printf("\nThe prefix expression:  "); preorder(infixExpressionTree(expression));
             
             if (digit_input(expression)){
-                printf("\n= ");
                 // eval using postfix
-                char* post = (char*)malloc(sizeof(char) * (strlen(expression))); char** ptrans = &post;
                 int c = 0; int* count = &c;
-
                 convert2postfix(infixExpressionTree(expression), strlen(expression), ptrans, count);
-                printf("%d\n", posteval(post));
+                // if (posteval(post) != -2147483648){ // division by 0
+                    printf("\n= ");
+                    printf("result %d...", posteval(post));
+                // }
             }
         }
+        printf("\n--------------------------------------------------\n");
+        free(expression);
+        memset(post, 0, sizeof(post));  // 要重制 string
     }
     return 0;
 }
+// 842+53-/*4425-+/+
